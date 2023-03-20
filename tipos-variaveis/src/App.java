08bca38469b83fff4b995f73689f0bb8e574999f
Kaitchin12 <cauaamorim05@gmail.com>
2023-03-20 16:01:28 -0300

    ./src/tipos-variaveis/app.java

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      int numero = 5;
+
+      numero = 10;
+
+      System.out.println(numero);
+
+      final double NOME_MEU = 18;
     }
 }

a36c3bb61d599d42a40d9ef3cc0613b746055838
Kaitchin12 <cauaamorim05@gmail.com>
2023-03-20 18:30:37 -0300

    operadores.java

--- tipos-variaveis/src/operadores.java
+++ tipos-variaveis/src/operadores.java
@@ -1,6 +1,8 @@
 public class operadores {
     
     public static void main(String[] args) {
-        
+            int numero = 5;
+            int numero2 = 6;
+                System.out.println(numero + numero2);
     }
 }

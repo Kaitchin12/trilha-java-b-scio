6301ccf1630a8e2e758b24bb3d18e4d960389073
Kaitchin12 <cauaamorim05@gmail.com>
2023-03-22 16:09:42 -0300

    launch.json

--- Java-Terminal/src/App.java
+++ Java-Terminal/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Estrutura para imprimir 
+        //nome
+        String nome = args[0];
+        //sobrenome
+        String sobrenome = args [1];
+        //idade, sendo int pois vamos colocar um numero 
+        int idade = Integer.valueOf(args[2]);
+        //altura, em double já que vamos colocar em "."
+        double altura = Double.valueOf(args[3]);
+
+        //impressão 
+        System.out.println("Olá me chamo" + nome + " " + sobrenome );
+        System.out.println("Tenho" + idade + "anos");
+        System.out.println("Minha altura é de " + altura + "cm");
     }
 }
